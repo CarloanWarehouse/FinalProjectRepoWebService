0048ff4dce2d47934947eae2f5af339501e13c0d
mahadevdhoran007 <mahadevdhoran007@gmail.com>
2023-02-15 14:40:49 +0530

    initial commit

---
+++ CarLoanWareHouseBackEnd/src/main/java/com/ware/main/CarLoanWareHouseBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ware.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLoanWareHouseBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLoanWareHouseBackEndApplication.class, args);
+	}
+
+}
---
+++ CarLoanWareHouseBackEnd/src/test/java/com/ware/main/CarLoanWareHouseBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ware.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarLoanWareHouseBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
